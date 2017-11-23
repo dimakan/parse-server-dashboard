cdf08fa6915bf4fe8c77805de00d6d506d777360
dimakan <sheldima@gmail.com>
2017-11-23 15:57:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // Example express application adding the parse-dashboard module to expose Parse Dashboard compatible API routes.
 
 var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
 var ParseDashboard = require('parse-dashboard');
 
 var path = require('path');
 
 var localParseServer = 'http://localhost:1337/parse';
+
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || localParseServer,  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+// Client-keys like the javascript key or the .NET key are not necessary with parse-server
+// If you wish you require them, you can set them as options in the initialization above:
+// javascriptKey, restAPIKey, dotNetKey, clientKey
+
 
 var dashboard = new ParseDashboard({
 
@@ -22,6 +39,13 @@ var dashboard = new ParseDashboard({
 
 var app = express();
 app.enable('trust proxy');
+
+// Serve static assets from the /public folder
+app.use('/public', express.static(path.join(__dirname, '/public')));
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
 
 // make the Parse Dashboard available at /
 app.use('/', dashboard);
